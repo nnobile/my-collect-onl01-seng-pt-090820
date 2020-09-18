7326c201b0ded953319d88de4f3edb1b1107723f
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 21:52:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
+list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-
+def my_collect(array)
+  counter = 0
+  new_array = []
+  while counter < array.length
+    new_array.push(array)
+    counter += 1
+  end
+end
