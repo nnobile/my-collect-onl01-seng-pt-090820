47ea0148562e8e45e22af143735f7b7c50fa1a89
Nick Nobile <nickcnobile@gmail.com>
2020-09-18 22:00:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   counter = 0
   new_array = []
   while counter < array.length
-    new_array.push(array) 
+    new_array.push(array)
     counter += 1
     yield(array[counter])
   end
